c0daae863ede48496889f434fbaa53204d53ae89
Deepak2004Bisht <deepakbisht2777@gmail.com>
2025-09-19 09:32:26 +0530

    Add Typed.js functionality and mobile menu toggle; create initial CSS styles
    - Implemented Typed.js for dynamic text display on the homepage.
    - Added event listener for mobile menu toggle functionality.
    - Created a new CSS file with styles for body, home section, skills, and media queries for responsive design.

---
+++ script.js
@@ -0,0 +1,18 @@
+ // Typed.js
+    new Typed("#typed-text", {
+      strings: ["Frontend Developer", "React", "MERN Stack Developer", "DSA Learner", "AI Learner"],
+      typeSpeed: 60,
+      backSpeed: 40,
+      loop: true
+    });
+
+
+    // Mobile Menu
+    document.getElementById("menuBtn").addEventListener("click", () => {
+      document.getElementById("mobileMenu").classList.toggle("hidden");
+    });
+
+
+    
+
+    
